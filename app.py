bba7b3f1c83f6163ad84a50b1c7142a297aacf74
Dateifi <dateifi@gmail.com>
2022-04-01 11:13:23 +0200

    Netflix-Clone 01.04.

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
+import os
+import uuid as uuid
+
 from flask import Flask, render_template, flash, url_for, redirect
 from flask_login import UserMixin, LoginManager, login_required, login_user, logout_user, current_user
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField
 from werkzeug.security import generate_password_hash, check_password_hash
-from wtforms import StringField, PasswordField, SubmitField
+from werkzeug.utils import secure_filename
+from wtforms import StringField, PasswordField, SubmitField, DateField
 from wtforms.validators import EqualTo, InputRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'MY SUPER SAVE KEY'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:4316@localhost/netflix_db'
+
+UPLOAD_FOLDER = 'static/images'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # Login functions
 
@@ -49,7 +57,37 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Sign in')
 
 
+class UpdateUserForm(FlaskForm):
+    name = StringField("Your name", validators=[InputRequired()])
+
+    submit = SubmitField('Submit')
+
+
 db = SQLAlchemy(app)
+
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    date_released = db.Column(db.Date())
+    img_url = db.Column(db.String(250))
+
+    def __init__(self, title, date_released, img_url):
+        self.title = title
+        self.date_released = date_released
+        self.img_url = img_url
+
+
+# foreign key
+# user_id = db.Column(db.Integer, db.ForeignKey('users_id')
+
+class AddMovieForm(FlaskForm):
+    title = StringField("Movie Title", validators=[InputRequired()],
+                        render_kw={'autofocus': True, 'placeholder': 'Movie Title'})
+    date_released = DateField('Release Date', validators=[InputRequired()])
+    img_url = FileField('Movie Image')
+    submit = SubmitField('Add Movie')
 
 
 class User(db.Model, UserMixin):
@@ -112,7 +150,7 @@ def dashboard():
 def register():
     form = RegisterForm()
     if current_user.is_authenticated:
-        return redirect(url_for('dashboard')) #function it
+        return redirect(url_for('dashboard'))  # function it
 
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
@@ -136,6 +174,37 @@ def logout():
     logout_user()
     flash('You have been logged out', 'success')
     return redirect(url_for('login'))
+
+
+@app.route('/update-profile/<int:id>', methods=['GET', 'POST'])
+@login_required
+def update_profile(id):
+    form = UpdateUserForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(id=id).first()
+        user.name = form.name.data
+        db.session.commit()
+        flash('Profile successfully updated!', 'success')
+        return redirect(url_for('dashboard'))
+    user = User.query.filter_by(id=id).first()
+    if user:
+        return render_template('update_user.html', form=form, user=user)
+
+
+@app.route('/add-movie', methods=['GET', 'POST'])
+@login_required
+def add_movie():
+    form = AddMovieForm()
+    if form.validate_on_submit():
+        img_filename = secure_filename(form.img_url.data.filename)
+        img_name = str(uuid.uuid1()) + '_' + img_filename
+        form.img_url.data.save(os.path.join(app.config['UPLOAD_FOLDER'], img_name))
+        movie = Movie(form.title.data, form.date_released.data, img_name )
+        db.session.add(movie)
+        db.session.commit()
+        flash('Movie successfully added', 'success')
+        return redirect(url_for('dashboard'))
+    return render_template('add_movie.html', form=form)
 
 
 
